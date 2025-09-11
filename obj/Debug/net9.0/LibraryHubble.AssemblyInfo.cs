3eb8ca98e3d7278f895a66f17b2f58d7384f2997
ju5nj0se <juaan.josehernandez@gmail.com>
2025-09-11 17:06:43 -0500

    add:file gitignore

--- obj/Debug/net9.0/LibraryHubble.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryHubble.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryHubble")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7226ed7bb6595c62ae36b8a6d345dba2250ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryHubble")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryHubble")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
