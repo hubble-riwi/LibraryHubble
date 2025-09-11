7b7226ed7bb6595c62ae36b8a6d345dba2250ca0
ju5nj0se <juaan.josehernandez@gmail.com>
2025-09-11 17:01:46 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿bool flag = true;
+string option;
+
+while (flag)
+{
+    Console.Write(@"===== SISTEMA DE BIBLIOTECA =====
+    1. Gestión de Libros
+    2. Gestión de Usuarios
+    3. Préstamos y Devoluciones
+    4. Reseñas y Calificaciones
+    5. Estadísticas
+    6. Salir
+    >> ");
+    option =  Console.ReadLine();
+    
+    switch (option)
+    {
+        case "1":
+
+            break;
+        
+        case "2":
+
+            break;
+        
+        case "3":
+
+            break;
+        
+        case "4":
+
+            break;
+        
+        case "5":
+
+            break;
+        
+        case "6":
+            flag = false;
+            break;
+        
+        default:
+            
+            Console.WriteLine("Opcion no valida");
+            break;
+    }
+}
